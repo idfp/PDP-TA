790c9c3b48a4205355f7002d06b130ec3bcc4b73
idfp <bashed@tuta.io>
2023-11-08 05:36:22 +0700

    add database files

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
 #include "stdio.h"
 
+void login(char* username, char* password){
+
+}
+
 int main(){
-    while(1){
-        int choice;
-        printf("Menu:\n1. Eksekusi\n2. Exit\n> ");
-        scanf("%d", &choice);
-        if(choice == 2){
-            break;
-        }
-    }
+    printf("  _____                              _ _    _____               \n");
+    printf(" |  __ \\                            | (_)  / ____|              \n");
+    printf(" | |__) |__ _ __ _ __ ___   __ _  __| |_  | |     __ _ _ __ ___ \n");
+    printf(" |  ___/ _ \\ '__| '_ ` _ \\ / _` |/ _` | | | |    / _` | '__/ _ \\\n");
+    printf(" | |  |  __/ |  | | | | | | (_| | (_| | | | |___| (_| | | |  __/\n");
+    printf(" |_|   \\___|_|  |_| |_| |_|\\__,_|\\__,_|_|  \\_____\\__,_|_|  \\___|\n");
+    printf("\nv. 0.1 Alpha\nLogin -\nMasukan Username:");
+                                                                
     return 0;
 }
